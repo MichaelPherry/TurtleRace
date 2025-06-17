1fd6cc1d42328ffaf8cf5f7b54d4d1ac5273b6b5
Kevin McKnight <kevinmcknight24@gmail.com>
2025-06-16 22:45:07 -0400

    Got the shop working, selects 4 random items with different raritys

--- Scenes/ItemList.gd
+++ Scenes/ItemList.gd
@@ -1,16 +1,29 @@
 extends ItemList
 var rng = RandomNumberGenerator.new()
-var weights = PackedFloat32Array([1, 1, 1, 1, 0.5])
+var Common = 64
+var Rare = 28
+var item_data = ItemData.item_data
+var item_rarity
+var random_item
 #weights will assign a weight to each index, ie index 0 has a weight of 1, index
 #4 has a weight of 0.5 meaning 1 is more likely to get picked
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(ItemData.item_data)
-	for item in ItemData.item_data:
+	print(item_data)
+	for item in range(4):
 		#var texture = load(item["ItemImage"]) as Texture2D
-		#print(item)
-		#add_item(item, texture)
-		pass
+		item_rarity = rng.randf_range(0, 100)
+		if item_rarity <= Common:
+			item_rarity = "Common"
+		elif item_rarity > Common && item_rarity <= (Common + Rare):
+			item_rarity = "Rare"
+		elif item_rarity > Common + Rare:
+			item_rarity = "Epic"
+
+		random_item = item_data[item_rarity].keys().pick_random()
+		print(random_item)
+		var texture = load(item_data[item_rarity][random_item]["ItemImage"]) as Texture2D
+		add_item(random_item, texture)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- Scripts/ItemData.gd
+++ Scripts/ItemData.gd
@@ -1,11 +1,13 @@
 extends Node
 
+var data_path = "res://Data/ItemData.json"
 var item_data = {}
 
 func _ready():
 	#This will open the ItemData file, read all the data, and add it into a dictonary
-	var item_data_file = FileAccess.open("res://Data/ItemData.json", FileAccess.READ)
-	var json = JSON.new()
-	var item_data_json = json.parse(item_data_file.get_as_text())
-	item_data_file.close()
+	var item_data_file = FileAccess.get_file_as_string(data_path)
+	var item_data_json = JSON.parse_string(item_data_file)
 	item_data = item_data_json
+	if(item_data == null):
+		push_warning("shit is empty bro")
+
